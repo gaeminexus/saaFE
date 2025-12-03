de650108ba043c1627aa5174e807602ede8120e9
xeonpotato <xeonpotato@gmail.com>
2025-12-03 00:08:31 -0500

    Arreglos en naturaleza de cuentas

--- src/app/modules/cnt/forms/naturaleza-cuentas/naturaleza-cuentas.component.ts
+++ src/app/modules/cnt/forms/naturaleza-cuentas/naturaleza-cuentas.component.ts
@@ -17,6 +17,7 @@ import { ExportService } from '../../../../shared/services/export.service';
 import { EntidadesContabilidad } from '../../model/entidades-cnt';
 import { NaturalezaCuenta } from '../../model/naturaleza-cuenta';
 import { NaturalezaCuentaService } from '../../service/naturaleza-cuenta.service';
+import { DetalleRubro } from '../../../../shared/model/detalle-rubro';
 
 // 🔑 Rubro para filtrar tipos de naturaleza (el valor seleccionado se almacena en el campo 'tipo' del formulario)
  const RUBRO_TIPO_GRUPO = 12;
@@ -43,6 +44,8 @@ export class NaturalezaDeCuentasComponent implements OnInit {
 
   tableConfig!: TableConfig;
 
+  tipoNaturaleza: DetalleRubro[] = [];
+
 
 
   constructor(
@@ -53,9 +56,8 @@ export class NaturalezaDeCuentasComponent implements OnInit {
 
   ngOnInit(): void {
     // Debug: Verificar detalles de rubro cargados
-    console.log(`📚 DetalleRubros cargados: ${this.detalleRubroService.estanDatosCargados()}`);
     if (this.detalleRubroService.estanDatosCargados()) {
-      const tiposDisponibles = this.detalleRubroService.getDetallesByParent(RUBRO_TIPO_GRUPO);
+      this.tipoNaturaleza = this.detalleRubroService.getDetallesByParent(RUBRO_TIPO_GRUPO);
     }
 
     this.loadData();
@@ -89,17 +91,10 @@ export class NaturalezaDeCuentasComponent implements OnInit {
   }
 
   private getRegConfig(): FieldConfig[] {
-    // Filtrar los tipos de naturaleza desde el rubro 13 del master
-    const tiposNaturaleza = this.detalleRubroService.getDetallesByParent(RUBRO_TIPO_GRUPO);
-
-    // Si no hay datos, mostrar todos los detalles disponibles para debugging
-    if (tiposNaturaleza.length === 0) {
-      const todosLosDetalles = this.detalleRubroService.getDetalles();
-    }
 
     // Transformar DetalleRubro a formato de opciones para el select
     // El valor seleccionado (codigoAlterno) se almacenará en el campo 'tipo'
-    const tiposOptions = tiposNaturaleza.map((detalle) => ({
+    const tiposOptions = this.tipoNaturaleza.map((detalle) => ({
       key: detalle.codigoAlterno,
       value: detalle.descripcion,
     }));
@@ -126,7 +121,7 @@ export class NaturalezaDeCuentasComponent implements OnInit {
         type: 'autocomplete',
         label: 'Tipo de Naturaleza',
         name: 'tipo',
-        collections: tiposNaturaleza,
+        collections: this.tipoNaturaleza,
         autocompleteType: 1,
         rubroAlterno: RUBRO_TIPO_GRUPO,
         selectField: ['descripcion'],
