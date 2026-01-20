4fcc13e9f528fa5b8d0d811280c2496fcb5f196a
GnNeckB <82963447+EduNeck@users.noreply.github.com>
2026-01-20 16:16:41 -0500

    fix(rrh): restaurar modelo y servicio de Departamentos

---
+++ src/app/modules/rrh/service/departemento-cargo.service.ts
@@ -0,0 +1,78 @@
+import { HttpHeaders, HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, catchError, of, throwError } from 'rxjs';
+import { ServiciosRhh } from './ws-rrh';
+import { departamentocargo } from '../model/departamento-cargo';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DepartementoCargoService {
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getAll(): Observable<departamentocargo[] | null> {
+    const wsGetById = '/getAll';
+    const url = `${ServiciosRhh.RS_DPRT}${wsGetById}`;
+    return this.http.get<departamentocargo[]>(url).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  getById(id: string): Observable<departamentocargo | null> {
+    const wsGetById = '/getId/';
+    const url = `${ServiciosRhh.RS_DPRT}${wsGetById}${id}`;
+    return this.http.get<departamentocargo>(url).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  /** POST: add new record */
+  add(datos: any): Observable<departamentocargo | null> {
+    return this.http.post<departamentocargo>(ServiciosRhh.RS_DPRT, datos, this.httpOptions).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  /** PUT: update record */
+  update(datos: any): Observable<departamentocargo | null> {
+    return this.http.put<departamentocargo>(ServiciosRhh.RS_DPRT, datos, this.httpOptions).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  selectByCriteria(datos: any): Observable<departamentocargo[] | null> {
+    const wsEndpoint = '/selectByCriteria/';
+    const url = `${ServiciosRhh.RS_DPRT}${wsEndpoint}`;
+    return this.http.post<any>(url, datos, this.httpOptions).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  /** DELETE */
+  delete(id: any): Observable<departamentocargo | null> {
+    const wsEndpoint = '/' + id;
+    const url = `${ServiciosRhh.RS_DPRT}${wsEndpoint}`;
+    return this.http.delete<departamentocargo>(url, this.httpOptions).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  private handleError(error: HttpErrorResponse): Observable<null> {
+    if (+error.status === 200) {
+      return of(null);
+    } else {
+      return throwError(() => error.error);
+    }
+  }
+
+
+}
+
+
